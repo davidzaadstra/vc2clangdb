6d760b4261c0d454e41f6436682a8d62114e17a8
David Zaadstra <david@zaadstra.de>
2014-07-14 01:00:19 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace vc2clangdb
+{
+	class Program
+	{
+		static string handleOption(string clOption)
+		{
+			if (clOption.StartsWith("D"))
+				return "-" + clOption;
+			else if (clOption.StartsWith("I"))
+				return "-" + clOption;
+			return "";
+		}
+
+		static string fixPath(string path)
+		{
+			return path.Replace('\\', '/');
+		}
+
+		static string escapeString(string str)
+		{
+			return fixPath(str);
+		}
+
+		static Dictionary<string, string> parseArgs(string[] args)
+		{
+			var dict = new Dictionary<string, string>();
+
+			try
+			{
+				for (int i = 0; i < args.Length; ++i)
+				{
+					if (args[i].StartsWith("-"))
+					{
+						dict.Add(args[i].Substring(1), args[i + 1]);
+						++i;
+					}
+					else
+						dict.Add("", args[i]);
+				}
+			}
+			catch(Exception)
+			{
+				Console.WriteLine("Error parsing arguments...");
+			}
+			return dict;
+		}
+
+		static void Main(string[] args)
+		{
+			var argDict = parseArgs(args);
+			if (!argDict.ContainsKey("") || !argDict.ContainsKey("i"))
+			{
+				Console.WriteLine("Usage: vc2clangdb <path-to-vcxproj> -i <intermediate-path> [options]\n");
+				Console.WriteLine("Options:");
+				Console.WriteLine("-o <output-json>   - Target path for the output json. If not specified, compile_commands.json will be created in the same location as the .vcxproj");
+				Console.WriteLine("-c <configuration> - Configuration name, e.g. 'Debug'. Currently only needed if intermediates do not exist. If not specified, msbuild will use the default");
+				Console.WriteLine("-p <platform>      - Platform name, e.g. 'Win32'. Currently only needed if intermediates do not exist. If not specified, msbuild will use the default");
+				return;
+			}
+			if(!File.Exists(args[0]))
+			{
+				Console.WriteLine(args[0] + " does not exist!");
+				return;
+			}
+
+			string proj = argDict[""];
+			string projDir = fixPath(Path.GetDirectoryName(proj));
+			string tlogPath = Path.Combine(argDict["i"], Path.GetFileNameWithoutExtension(proj) + ".tlog\\cl.command.1.tlog");
+			string target = argDict.ContainsKey("o") ? argDict["o"] : (fixPath(Path.Combine(projDir, "compile_commands.json")));
+
+			if (!File.Exists(tlogPath) || File.GetLastWriteTimeUtc(tlogPath) < File.GetLastWriteTimeUtc(proj))
+			{
+				var vs = VisualStudio.Find(12); // TODO: extract version from vcxproj
+				vs.Build(proj, argDict.ContainsKey("p") ? argDict["p"] : null, argDict.ContainsKey("c") ? argDict["c"] : null, argDict["i"]);
+				if(!File.Exists(tlogPath))
+					vs.Rebuild(proj, argDict.ContainsKey("p") ? argDict["p"] : null, argDict.ContainsKey("c") ? argDict["c"] : null, argDict["i"]);
+			}
+
+			string[] lines = File.ReadAllLines(tlogPath);
+			int index = 0;
+			string compilationDatabase = "[";
+			for(int i = 0; i < lines.Length / 2; ++i, ++index)
+			{
+				string clangCommandline = "clang++ ";
+
+				string filename = fixPath(lines[index++].Substring(1));
+				string commandline = lines[index];
+
+				// this is not robust. if we encounter a / within a string, we might run into problems
+				string[] options = commandline.Split(new char[] { '/' });
+				foreach (string option in options)
+					clangCommandline += escapeString(handleOption(option));
+
+				clangCommandline += " -fms-compatibility";
+				clangCommandline += " " + filename;
+				
+				compilationDatabase += "\n\t{\n\t\t";
+				compilationDatabase += "\"directory\": \"" + projDir + "\",\n\t\t";
+				compilationDatabase += "\"command\": \"" + clangCommandline + "\",\n\t\t";
+				// TODO: relative filename
+				compilationDatabase += "\"file\": \"" + filename + "\"\n\t}";
+				if(lines.Length - index > 2)
+					compilationDatabase += ",";
+			}
+			compilationDatabase += "\n]";
+
+			File.WriteAllText(target, compilationDatabase);
+		}
+	}
+}
---
+++ VisualStudio.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+using System.IO;
+using System.Diagnostics;
+
+namespace vc2clangdb
+{
+	public class VisualStudio
+	{
+		// This is not at all robust yet! It was tested on my own express machine only. The paths suggested online
+		// do not exist, so I searched the registry myself. Major versions other than 12 might have completely different
+		// paths.
+		public static VisualStudio Find(int majorVersion)
+		{
+			string path = null;
+			foreach (bool express in new bool[] { false, true })
+			{
+				string registryKeyString = String.Format(@"SOFTWARE\{0}\Microsoft\{1}\{2}.0\Setup\VS",
+				   Environment.Is64BitProcess ? @"Wow6432Node" : "",
+				   express ? "WDExpress" : "VisualStudio",
+				   majorVersion);
+
+				using (RegistryKey localMachineKey = Registry.LocalMachine.OpenSubKey(registryKeyString))
+				{
+					path = localMachineKey.GetValue("ProductDir") as string;
+				}
+				if (!String.IsNullOrEmpty(path))
+					break;
+			}
+			return new VisualStudio(path, majorVersion);
+		}
+
+		public string PreprocessProject(string projectPath)
+		{
+			var msbuild = new Process();
+			msbuild.StartInfo.Arguments = "/property:PlatformName=x64 /preprocess " + projectPath;
+			msbuild.StartInfo.FileName = GetMSBuild();
+			msbuild.StartInfo.RedirectStandardOutput = true;
+			msbuild.StartInfo.UseShellExecute = false;
+			msbuild.Start();
+			string output = msbuild.StandardOutput.ReadToEnd();
+
+			// now we could parse the project, but that's complicated. we need to evaluate expressions etc
+			// also, we need to ask the user for Configuration and Platform
+
+			throw new NotImplementedException();
+		}
+
+		public void Build(string projectPath, string platform, string configuration, string intermediateDir)
+		{
+			RunMSBuild(projectPath, BuildPropString(platform, configuration, intermediateDir));
+		}
+
+		public void Rebuild(string projectPath, string platform, string configuration, string intermediateDir)
+		{
+			Console.WriteLine(RunMSBuild(projectPath, "/target:Rebuild" + BuildPropString(platform, configuration, intermediateDir)));
+		}
+
+		private VisualStudio(string path, int majorVersion)
+		{
+			m_path = path;
+			m_majorVersion = majorVersion;
+		}
+
+		private string RunMSBuild(string projectPath, string args)
+		{
+			var msbuild = new Process();
+			msbuild.StartInfo.WorkingDirectory = Path.GetDirectoryName(GetMSBuild());
+			msbuild.StartInfo.FileName = GetMSBuild();
+			msbuild.StartInfo.Arguments = projectPath + " " + args;
+			msbuild.StartInfo.RedirectStandardOutput = true;
+			msbuild.StartInfo.UseShellExecute = false;
+			msbuild.Start();
+			return msbuild.StandardOutput.ReadToEnd();
+		}
+
+		private string GetMSBuild()
+		{
+			return Path.Combine(m_path, String.Format("../msbuild/{0}.0/bin/msbuild.exe", m_majorVersion));
+		}
+
+		private static string BuildPropString(string platform, string configuration, string intermediateDir)
+		{
+			var props = new List<string>();
+			if (!String.IsNullOrEmpty(platform))
+				props.Add("PlatformName=" + platform);
+			if (!String.IsNullOrEmpty(configuration))
+				props.Add("Configuration=" + configuration);
+			if (!String.IsNullOrEmpty(intermediateDir))
+				props.Add("IntDir=" + intermediateDir);
+			string propArg = "";
+			if (props.Count > 0)
+				propArg = " /property:" + String.Join(";", props);
+
+			return propArg;
+		}
+
+		private string m_path;
+		private int m_majorVersion;
+	}
+}
